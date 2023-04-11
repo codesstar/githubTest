1bb00589e73818a7a458dad088298aa504eb0c48
tom_Pro <goodMoring_pro@atguigu.com>
2023-04-11 09:05:34 +0800

    this is the second

---
+++ src/nihao.java
@@ -0,0 +1,7 @@
+public class nihao {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/xixiix.java
@@ -0,0 +1,5 @@
+public class xixiix {
+    public static void main(String[] args) {
+        System.out.println("xxx");
+    }
+}
